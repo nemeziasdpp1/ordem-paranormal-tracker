dffd4e99052fe9ae6a3196c37147f04bb6cc8f4c
nemeziasdpp1 <nemezesdpp1@gmail.com>
2026-06-18 21:34:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+const personagens = [
+  {
+    nome: "Dio",
+    pv: "20/20",
+    san: "15/15",
+    pe: "10/10",
+    iniciativa: 12
+  }
+];
 
+const container = document.getElementById("container");
+
+function render() {
+  container.innerHTML = "";
+
+  personagens
+    .sort((a, b) => b.iniciativa - a.iniciativa)
+    .forEach(p => {
+      const div = document.createElement("div");
+      div.className = "card";
+
+      div.innerHTML = `
+        <strong>${p.nome}</strong><br>
+        PV: ${p.pv}<br>
+        SAN: ${p.san}<br>
+        PE: ${p.pe}<br>
+        Iniciativa: ${p.iniciativa}
+      `;
+
+      container.appendChild(div);
+    });
+}
+
+render();
