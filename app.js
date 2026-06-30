4c623706af5dfa6a05f78d51fd567b927e12dfcf
nemeziasdpp1 <nemezesdpp1@gmail.com>
2026-06-30 15:21:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ let abaModalHabAtiva = "";
 let subFiltroModalHabAtivo = "";
 let idsHabilidadesExpandidas = new Set();
 
-import { aplicarBonusDeHabilidades } from './js/regras.js';
+import { aplicarBonusDeHabilidades } from './regras.js';
 // --- NOVA LÓGICA DE DEFESA ---
 window.calcularDefesas = () => {
     const p = obterPersonagemAtual();
