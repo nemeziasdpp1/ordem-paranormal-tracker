b63cf1668f4a6f7ce317fb8675ec3defaf44a52d
nemeziasdpp1 <nemezesdpp1@gmail.com>
2026-06-30 18:47:52 -0300

    Update regras.js

--- regras.js
+++ regras.js
@@ -1,12 +1,19 @@
 export const regrasHabilidades = {
+    
     "Calejado": (p) => {
         const nex = Number(p.nex) || 0;
         const bonusPV = Math.floor(nex / 5);
-        
         if (!p.status) p.status = {};
-
         p.status.pvMax = (Number(p.status.pvMax) || 0) + bonusPV; 
+    },
+    
+    "Cicatrizes Psicológicas": (p) => {
+        const nex = Number(p.nex) || 0;
+        const bonusSAN = Math.floor(nex / 5);
+        if (!p.status) p.status = {};
+        p.status.sanMax = (Number(p.status.sanMax) || 0) + bonusSAN; 
     }
+
 };
 
 export function aplicarBonusDeHabilidades(p) {
