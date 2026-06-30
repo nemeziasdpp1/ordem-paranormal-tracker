bc0cc2cc3f3315c756145770a8fdb9c00239d5a9
nemeziasdpp1 <nemezesdpp1@gmail.com>
2026-06-30 18:20:35 -0300

    Update regras.js

--- regras.js
+++ regras.js
@@ -1,13 +1,10 @@
-// js/regras.js
-
 export const regrasHabilidades = {
     "Calejado": (p) => {
         const nex = Number(p.nex) || 0;
         const bonusPV = Math.floor(nex / 5);
         
         if (!p.status) p.status = {};
-        
-        // A mágica acontece aqui: somando no p.status.pvMax
+
         p.status.pvMax = (Number(p.status.pvMax) || 0) + bonusPV; 
     }
 };
